4832f9083e24af4c58478fb7f76f5ebbb4fb15cd
L-TYM-L <berglus@mail.ru>
2022-05-01 21:52:06 +0300

    up12

--- Source/MultiplayerShooter/ShooterGameSession.cpp
+++ Source/MultiplayerShooter/ShooterGameSession.cpp
@@ -9,27 +9,27 @@ void AShooterGameSession::RegisterServer()
 {
 	Super::RegisterServer();
 
-	UE_LOG(LogTemp, Log, TEXT("Registering server and creating session --------"));
-	IOnlineSubsystem* OnlineSubSystem = IOnlineSubsystem::Get();
-	if (OnlineSubSystem == nullptr) {
-		UE_LOG(LogTemp, Error, TEXT("No online subsystem found!!"));
-		return;
-	}
-	UE_LOG(LogTemp, Log, TEXT("Got online subsystem"));
-	IOnlineSessionPtr Session = OnlineSubSystem->GetSessionInterface();
-	UE_LOG(LogTemp, Log, TEXT("Got session interface"));
-	FOnlineSessionSettings Settings;
+	//UE_LOG(LogTemp, Log, TEXT("Registering server and creating session --------"));
+	//IOnlineSubsystem* OnlineSubSystem = IOnlineSubsystem::Get();
+	//if (OnlineSubSystem == nullptr) {
+	//	UE_LOG(LogTemp, Error, TEXT("No online subsystem found!!"));
+	//	return;
+	//}
+	//UE_LOG(LogTemp, Log, TEXT("Got online subsystem"));
+	//IOnlineSessionPtr Session = OnlineSubSystem->GetSessionInterface();
+	//UE_LOG(LogTemp, Log, TEXT("Got session interface"));
+	//FOnlineSessionSettings Settings;
 
-	Settings.bIsDedicated = true;
-	Settings.bIsLANMatch = false;
-	Settings.bAllowJoinInProgress = true;
-	Settings.bShouldAdvertise = true;
-	Settings.NumPublicConnections = 64;
-	Settings.bUsesPresence = true;
+	//Settings.bIsDedicated = true;
+	//Settings.bIsLANMatch = false;
+	//Settings.bAllowJoinInProgress = true;
+	//Settings.bShouldAdvertise = true;
+	//Settings.NumPublicConnections = 32;
+	//Settings.bUsesPresence = true;
 
-	UE_LOG(LogTemp, Log, TEXT("Initialized settings"));
+	//UE_LOG(LogTemp, Log, TEXT("Initialized settings"));
 
-	Session->CreateSession(0, GameSessionName, Settings);
+	//Session->CreateSession(0, GameSessionName, Settings);
 
-	UE_LOG(LogTemp, Log, TEXT("Session created --------")); 
+	//UE_LOG(LogTemp, Log, TEXT("Session created --------")); 
 }
